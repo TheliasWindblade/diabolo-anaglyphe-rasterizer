6aa4af708bb9184ae5095019167363683714ed0e
Alexis <alexis.cesaro.ac@gmail.com>
2020-01-29 12:28:44 +0100

    Project init

---
+++ main.cpp
@@ -0,0 +1,47 @@
+//
+// Created by Alexis on 29/01/2020.
+//
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <limits>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include "vector3.h"
+
+
+
+void render() {
+    const int   width    = 1024;
+    const int   height   = 768;
+    const float fov      = M_PI/3.;
+    std::vector<Vec3f> framebuffer(width*height);
+
+#pragma omp parallel for
+    for (size_t j = 0; j<height; j++) { // actual rendering loop
+        for (size_t i = 0; i<width; i++) {
+            std::cout << "Color" << std::endl;
+        }
+    }
+
+    std::ofstream ofs; // save the framebuffer to file
+    ofs.open("./out.ppm",std::ios::binary);
+    ofs << "P6\n" << width << " " << height << "\n255\n";
+    for (size_t i = 0; i < height*width; ++i) {
+        Vec3f &c = framebuffer[i];
+        float max = std::max(c[0], std::max(c[1], c[2]));
+        if (max>1) c = c*(1./max);
+        for (size_t j = 0; j<3; j++) {
+            ofs << (char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
+        }
+    }
+    ofs.close();
+}
+
+int main() {
+    render();
+    return 0;
+}
+
---
+++ vector3.h
@@ -0,0 +1,24 @@
+//
+// Created by Alexis on 29/01/2020.
+//
+
+#ifndef __GEOMETRY_H__
+#define __GEOMETRY_H__
+
+#include <cmath>
+#include <vector>
+#include <cassert>
+#include <iostream
+
+typedef vec<3, float> Vec3f;
+
+
+template <typename T> struct vec<3,T> {
+    vec() : x(T()), y(T()), z(T()) {}
+    vec(T X, T Y, T Z) : x(X), y(Y), z(Z) {}
+    T& operator[](const size_t i)       { assert(i<3); return i<=0 ? x : (1==i ? y : z); }
+    const T& operator[](const size_t i) const { assert(i<3); return i<=0 ? x : (1==i ? y : z); }
+    float norm() { return std::sqrt(x*x+y*y+z*z); }
+    vec<3,T> & normalize(T l=1) { *this = (*this)*(l/norm()); return *this; }
+    T x,y,z;
+};

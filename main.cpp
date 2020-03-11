8ec039ae697d16f3a7a630c40ab876c2386fdd8a
Thelias <mercuryelements@gmail.com>
2020-03-11 05:06:46 +0000

    zbuffer continued : still not working as intended but closer (plus rounding correction)

--- main.cpp
+++ main.cpp
@@ -20,17 +20,23 @@ Model* model=NULL;
 /**
  * Transform .obj coordinates [-1,1] into discrete image coordinates [0,width|height]
  */
-Vec2i CObjToImage(float x, float y){
-  int x0 = (x+1.)*width/2.;
-  int y0 = (y+1.)*height/2.;
-  return Vec2i(x0,y0);
+Vec3f CObjToImage(Vec3f v){
+  int x0 = ((v.x+1.)*width/2.+.5);
+  int y0 = ((v.y+1.)*height/2.+.5);
+  return Vec3f(x0,y0,v.z);
 }
 
-Vec2i CObjToImage(Vec3f v){
-  int x0 = (v.x+1.)*width/2.;
-  int y0 = (v.y+1.)*height/2.;
-  return Vec2i(x0,y0);
+/**
+ * Convert image coordinates[0,width|height] to 1D framebuffer coordinates [0,width*height]
+ */
+int CImageToZBuffer(int x, int y){
+  return x*height+y;
 }
+
+int CImageToZBuffer(Vec3f v){
+  return v.x*height+v.y;
+}
+
 
 /**
  * Draw a line between (x0,y0) and (x1,y1). Who could've guessed.
@@ -60,30 +66,39 @@ void line(Vec2i p0, Vec2i p1, Image *image, Vec3f color){
   }
 }
 
+void line(Vec3f p0, Vec3f p1, Image *image, Vec3f color){
+  line(Vec2i(p0.x,p0.y),Vec2i(p1.x,p1.y),image,color);
+}
+
 /**
  * Draw a triangle, filled with given color.
  */
-void bayesian_triangle(Vec2i p0, Vec2i p1, Vec2i p2, Image *image, Vec3f color){
-  Triangle2i tri(p0,p1,p2);
-  Vec2i bbox_clamp(image->width()-1,image->height()-1);
-  Vec2i bbox_min(image->width()-1,image->height()-1);
-  Vec2i bbox_max(0,0);
+void bayesian_triangle(Triangle3f tri, Image *image, float *zbuffer, Vec3f color){
+  Vec2f bbox_clamp(image->width()-1.,image->height()-1.);
+  Vec2f bbox_min(std::numeric_limits<float>::max(),std::numeric_limits<float>::max());
+  Vec2f bbox_max(std::numeric_limits<float>::min(),std::numeric_limits<float>::min());
   for(int i=0 ; i < 3 ; i++){
     for(int j=0; j < 2 ; j++){
-      bbox_min[j]=std::max(0,std::min(bbox_min[j],tri[i][j]));
+      bbox_min[j]=std::max(0.f,std::min(bbox_min[j],tri[i][j]));
       bbox_max[j]=std::min(bbox_clamp[j],std::max(bbox_max[j],tri[i][j]));
     }
   }
-  Vec2i v;
+  Vec3f v;
   for(v.x=bbox_min.x;v.x<=bbox_max.x;v.x++){
     for(v.y=bbox_min.y;v.y<=bbox_max.y;v.y++){
       Vec3f bc = tri.barycentric(v);
       if(bc.x<0||bc.y<0||bc.z<0) continue;
-      image->setPixel(v.x,v.y,color);
+      v.z=0;
+      for(int i=0;i<3;i++) { v.z += tri[i].z*bc[i]; }
+      if(zbuffer[CImageToZBuffer(v)]<v.z||1){
+	zbuffer[CImageToZBuffer(v)]=v.z;
+	image->setPixel(v.x,v.y,color);
+      }
     }
   }
 }
 
+//DEPRECATED : Render a triangle using the old scanlines method.
 void scanlines_triangle(Vec2i p0, Vec2i p1, Vec2i p2, Image *image, Vec3f color){
   if(p0.y==p1.y && p0.y==p2.y) return;
   if(p0.y > p1.y) std::swap(p0,p1);
@@ -104,11 +119,11 @@ void scanlines_triangle(Vec2i p0, Vec2i p1, Vec2i p2, Image *image, Vec3f color)
   }
 }
 
-void triangle(Vec2i p0, Vec2i p1, Vec2i p2, Image *image, Vec3f color){
-  //line(p0,p1,image,Vec3f(0.5,0.5,0.5));
-  //line(p2,p1,image,Vec3f(0.5,0.5,0.5));
-  //line(p0,p2,image,Vec3f(0.5,0.5,0.5));
-  bayesian_triangle(p0,p1,p2,image,color);
+//Render a triangle in wireframe.
+void wireframe(Triangle3f tri,Image *image, Vec3f color){
+  line(tri.v0,tri.v1,image,color);
+  line(tri.v1,tri.v2,image,color);
+  line(tri.v0,tri.v2,image,color);
 }
 
 /**
@@ -116,30 +131,44 @@ void triangle(Vec2i p0, Vec2i p1, Vec2i p2, Image *image, Vec3f color){
  */
 void render() {
   Image* framebuffer = new Image(width,height);
+  float zbuffer[width*height];
+  for(int i=0;i<width*height;i++){
+    zbuffer[i]=std::numeric_limits<float>::min();
+  }
   Vec3f light_dir(0,0,-1);
   //Render the model
   for(int f=0;f<model->nfaces();f++){
     std::vector<int> face = model->getFaceVertexes(f);
-    Vec2i screen_coords[3];
+    Vec3f screen_coords[3];
     Vec3f world_coords[3];
     for(int i=0;i<3;i++){
       Vec3f v = model->getVertex(face[i]);
-      screen_coords[i] = Vec2i((v.x+1.)*width/2.,(v.y+1)*width/2.);
+      screen_coords[i] = CObjToImage(v);
       world_coords[i]=v;
     }
     Vec3f n = cross(world_coords[2]-world_coords[0],world_coords[1]-world_coords[0]);
     n.normalize();
     float intensity = n*light_dir;
     if(intensity<=0) continue;
-    Vec3f color(((rand()%255)*intensity)/255.,((rand()%255)*intensity)/255.,((rand()%255)*intensity)/255.);
-    triangle(screen_coords[0],screen_coords[1],screen_coords[2],framebuffer,color);
+    Vec3f color((128+(rand()%128)*intensity)/255.,(128+(rand()%128)*intensity)/255.,(128+(rand()%128)*intensity)/255.);
+    //color=Vec3f(intensity,intensity,intensity);
+    Triangle3f tri(screen_coords[0],screen_coords[1],screen_coords[2]);
+    bayesian_triangle(tri,framebuffer,zbuffer,color);
+    //wireframe(tri,framebuffer,Vec3f(0.66,0.13,0.45));
   }
 
-  //Test renders
-  //triangle(Vec2i(180,50),Vec2i(150,1),Vec2i(70,180),framebuffer,Vec3f(1,0.5,0.5));
-
-  //Save the image.
-  framebuffer->saveAsPPM();
+  //Print z-buffer
+  Image* zbuffer_r = new Image(width,height);
+  for(int y=0 ; y < height ; y ++){
+    for(int x=0 ; x < width ; x ++){
+      float c = zbuffer[CImageToZBuffer(x,y)];
+      zbuffer_r->setPixel(x,y,Vec3f(c,c,c));
+    }
+  }
+  
+  //Save the images
+  framebuffer->saveAsPPM("out.ppm");
+  zbuffer_r->saveAsPPM("z.ppm");
 
   delete model;
   delete framebuffer;
@@ -149,7 +178,7 @@ int main(int argc, char* argv[]) {
   if(argc==2){
     model = new Model(argv[1]);
   } else {
-    model = new Model("obj/diablo3_pose.obj");
+    model = new Model("./obj/diablo3_pose.obj");
   }
   render();
   return 0;
